a4edc996727e050ca286c76f28b58fa92da255c7
KDS01 <rlagnek12@naver.com>
2023-08-25 18:17:42 +0900

    added Cellphone, SmallEnv

---
+++ src/provisoChoice/CellphoneProviso.java
@@ -0,0 +1,15 @@
+package provisoChoice;
+
+public class CellphoneProviso extends ChoiceObject {
+	{
+		this.choicename ="휴대폰";
+		this.choiceObjectLines = "평범한 갤럭시 휴대폰이다.오래 된 모델인데다가 액정의 오른쪽 귀퉁이에 금이 가 있다. 하지만 여전히 문제없이 작동한다\r\n"
+				+ "위이잉\r\n"
+				+ "휴대폰을 집어 올리다가 화면이 눌렸는지 옅은 진동이 느껴졌다.\r\n"
+				+ "삑, 삑 하는 기계음이 멀리서 들리더니, 바퀴 끌리는 소리를 내며 작은 장난감이 확 튀어나왔다.\r\n"
+				+ "\"이건.....\""
+				+ "아래쪽에 바퀴가 달린, 작은 쥐 모양의 장난감이 구석에서 튀어나왔다 ";
+		this.addProviso("단서 : 휴대폰");
+	}
+
+}
---
+++ src/provisoChoice/SmallenvelopProviso.java
@@ -0,0 +1,9 @@
+package provisoChoice;
+
+public class SmallenvelopProviso extends ChoiceObject {
+	{
+		this.choicename = "작은 봉투";
+		this.choiceObjectLines = "무엇이 들어있는지 모를 작은 봉투다. 언제부터 내 가방에 있었더라.. 뭐가 들어있을 지 모르니 조심스럽게 열어보자";
+	}
+
+}
